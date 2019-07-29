6365d162389d300f7119596672bcf357b14d3682
Andrew Fleming <andrew.fleming@automattic.com>
2019-07-29 15:03:48 +1000

    Andrew Fleming's packages and apps

--- install/brew.sh
+++ install/brew.sh
@@ -1,24 +1,16 @@
 # Install Homebrew
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap homebrew/versions
-brew tap homebrew/dupes
-brew tap Goles/battery
-brew tap homebrew/php
 brew update
 brew upgrade
 
 # Install packages
-brew install dockutil
-brew install htop
 brew install iftop
 brew install openssl
 brew install git
 brew install node
 brew install python3
 brew install wget
-brew install yarn
-brew install unrar
 brew install wifi-password
 
 # Wait a bit before moving on...
@@ -38,15 +30,13 @@ brew cask install --appdir="/Applications" google-chrome
 brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" slack
 brew cask install --appdir="/Applications" spotify
-brew cask install --appdir="/Applications" visual-studio-code
+brew cask install --appdir="/Applications" phpstorm
 brew cask install --appdir="/Applications" postman
 brew cask install --appdir="/Applications" spectacle
-brew cask install --appdir="/Applications" appcleaner
-brew cask install --appdir="/Applications" the-unarchiver
 brew cask install --appdir="/Applications" iterm2
-brew cask install --appdir="/Applications" franz
 brew cask install --appdir="/Applications" dropbox
-brew cask install --appdir="/Applications" keeweb
+brew cask install --appdir="/Applications" 1password
+brew cask install --appdir="/Applications" typora
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook qlvideo
